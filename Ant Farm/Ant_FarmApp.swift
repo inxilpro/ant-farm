25ee0dabe2daf85e4d57fe20b5dcda110d2b7e9f
Chris Morrell <inxilpro@users.noreply.github.com>
2026-09-25 17:57:10 -0400

    Initial Commit

---
+++ Ant Farm/Ant_FarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ant_FarmApp.swift
+//  Ant Farm
+//
+//  Created by Chris Morrell on 9/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Ant_FarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
